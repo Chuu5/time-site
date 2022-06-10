d204c5cb48d0a50d2ab074718fde56cf27a9f3ac
Chuu <99422689+Chuu5@users.noreply.github.com>
2022-06-10 15:50:05 -0300

    fixing stuff

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,8 +1,9 @@
 const increment = document.querySelectorAll(".increment")
 const value = document.querySelectorAll(".number")
 const decrement = document.querySelectorAll(".decrement")
-const purchase = document.querySelectorAll(".submit")
-const thanks = document.querySelector(".thanks")
+const purchase = document.querySelectorAll(".submit-button")
+const thanks = document.querySelector(".beeg .thanks")
+const thanksInMedia = document.querySelectorAll(".thanks")
 
 let prices = [10, 50, 200]
 let count = [0, 0, 0]
@@ -11,9 +12,9 @@ let count = [0, 0, 0]
 for(let i = 0; i < purchase.length; i++) {
 
     purchase[i].addEventListener("click", () => {
-        if(count[i] > 0) {
-            thanks.innerHTML = `<p>Thanks for purchasing our time</p>`
-            thanks.innerHTML += `<p class="opacity">Price: $${count[i] * prices[i]}</p>`
+
+        if(count[i] > 0 && body.clientWidth > 600) {
+            thanks.innerHTML = `<p>Thanks for purchasing our time</p><p class="opacity">Price: $${count[i] * prices[i]}</p>`
             body.classList.add("appear")
             value[i].innerHTML = 0
             count[i] = 0
@@ -23,7 +24,22 @@ for(let i = 0; i < purchase.length; i++) {
                 
             }, 3000)
 
+        } else if (count[i] > 0 && body.clientWidth <= 600) {
+            thanksInMedia[i].classList.add("media")
+
+            thanksInMedia[i].innerHTML = `<p>Thanks for purchasing our time</p><p class="opacity">Price: $${count[i] * prices[i]}</p>`
+            body.classList.add("appear")
+            value[i].innerHTML = 0
+            count[i] = 0
+            setTimeout( () => {
+                body.classList.remove("appear")
+                thanksInMedia[i].innerHTML = ""
+                thanksInMedia[i].classList.remove("media")
+            }, 3000)
+        } else {
+            alert("You can't buy 0 products")
         }
+        
     })
 }
 
