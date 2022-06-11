e84a9b837ac13ba60e8376411ba5a7a2c9e3722f
Chuu <99422689+Chuu5@users.noreply.github.com>
2022-06-11 19:32:50 -0300

    FIXED!

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -4,6 +4,7 @@ const decrement = document.querySelectorAll(".decrement")
 const purchase = document.querySelectorAll(".submit-button")
 const thanks = document.querySelector(".beeg .thanks")
 const thanksInMedia = document.querySelectorAll(".thanks")
+const beeg = document.querySelector(".thanks")
 
 let prices = [10, 50, 200]
 let count = [0, 0, 0]
@@ -14,28 +15,36 @@ for(let i = 0; i < purchase.length; i++) {
     purchase[i].addEventListener("click", () => {
 
         if(count[i] > 0 && body.clientWidth > 950) {
+
             thanks.innerHTML = `<p>Thanks for purchasing our time</p><p class="opacity">Price: $${count[i] * prices[i]}</p>`
+
             body.classList.add("appear")
+
             value[i].innerHTML = 0
             count[i] = 0
+
             setTimeout( () => {
                 body.classList.remove("appear")
                 thanks.innerHTML = ""
                 
-            }, 3000)
+            }, 2000)
 
         } else if (count[i] > 0 && body.clientWidth <= 950) {
+
             thanksInMedia[i].classList.add("media")
 
             thanksInMedia[i].innerHTML = `<p>Thanks for purchasing our time</p><p class="opacity">Price: $${count[i] * prices[i]}</p>`
             body.classList.add("appear")
+            
             value[i].innerHTML = 0
             count[i] = 0
+
             setTimeout( () => {
                 body.classList.remove("appear")
                 thanksInMedia[i].innerHTML = ""
                 thanksInMedia[i].classList.remove("media")
-            }, 3000)
+            }, 2000)
+            
         } else {
             alert("You can't buy 0 products")
         }
